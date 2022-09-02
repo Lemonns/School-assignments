dec0c349a4c0695ff3d6613850287c4a663371c6
Trey <104473511+Lemonns@users.noreply.github.com>
2022-09-02 11:46:22 -0500

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -1,35 +1,42 @@
 //HW #2: Trey Marlette
 #include <stdio.h>
 
+
 int main(void)
 {
     printf("HW #2: Trey Marlette\n");
 
     float userInput;
-    float pints = .5;
-    float ounces = 8;
-    float tablespoons = 2 * ounces;
-    float teaspoons = 3 * tablespoons;
 
+    //Conversions
+    const float PINTS = 2;
+    const float OUNCES = 8;
+    const float TABLESPOONS = 2 * OUNCES;
+    const float TEASPOONS = 3 * TABLESPOONS;
+    
     printf("Enter the number of cups to be converted.  ");
-    scanf("%f", &userInput); //gets number of cups from user input
+    scanf("%f", &userInput); //Gets number of cups from user input
 
-    printf("%8.2f cups = %10.3f pints\n", userInput, userInput / 2); //formats and prints cups and pints
-    printf("%*s= %10.3f ounces\n", 14, "", userInput * ounces); //formats and prints ounces
-    printf("%*s= %10.3f tablespoons\n", 14, "", 51.200); //formats and prints tablespoons
-    printf("%*s= %10.3f teaspoons\n", 14, "", 55555.600); //formats and prints teaspoons
+    printf("%8.2f cups = %10.3f pints\n", userInput, userInput / PINTS); //Formats and prints cups and pints
+    printf("%*s= %10.3f ounces\n", 14, "", userInput * OUNCES); //Formats and prints ounces
+    printf("%*s= %10.3f tablespoons\n", 14, "", userInput * TABLESPOONS); //Formats and prints tablespoons
+    printf("%*s= %10.3f teaspoons\n", 14, "", userInput * TEASPOONS ); //Formats and prints teaspoons
     
 
     return 0;
 }
 
 
+
 //Example output
 
 /*    
-Enter the number of cups to be converted.  5
-    5.00 cups =      2.500 pints
+
+HW #2: Trey Marlette
+Enter the number of cups to be converted.  3.2
+    3.20 cups =      1.600 pints
               =     25.600 ounces
               =     51.200 tablespoons
-              =  55555.600 teaspoons
+              =    153.600 teaspoons
+
 */
